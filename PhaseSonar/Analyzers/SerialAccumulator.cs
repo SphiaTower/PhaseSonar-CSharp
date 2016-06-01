467d9a2b40579cd24d5c70fd3800ebb9ef3c2c7d
SphiaTower <gwaterwall@gmail.com>
2016-06-01 22:49:51 +0800

    a trivial update

---
+++ PhaseSonar/Analyzers/SerialAccumulator.cs
@@ -0,0 +1,31 @@
+﻿using PhaseSonar.Correctors;
+using PhaseSonar.Slicers;
+
+namespace PhaseSonar.Analyzers
+{
+    /// <summary>
+    ///     A serial accumulator that process pulses one by one.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class SerialAccumulator<T> : Accumulator<T> where T : ISpectrum
+    {
+        /// <summary>
+        ///     Create a serial accumulator.
+        /// </summary>
+        /// <param name="slicer">
+        ///     <see cref="ISlicer" />
+        /// </param>
+        /// <param name="corrector">
+        ///     <see cref="ICorrector{T}" />
+        /// </param>
+        public SerialAccumulator(ISlicer slicer, ICorrector<T> corrector) : base(slicer)
+        {
+            Strategy = new SerialStrategy<T>( corrector);
+        }
+
+        /// <summary>
+        ///     <see cref="IAnalyzerStrategy{T}" />
+        /// </summary>
+        protected override IAnalyzerStrategy<T> Strategy { get; set; }
+    }
+}
